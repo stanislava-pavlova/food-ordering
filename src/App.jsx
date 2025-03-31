14f706a7571a6f468268340d6863a93ec8af7d3d
stanislava-pavlovaa <stanislavagpavlova@gmail.com>
2025-03-31 21:53:02 +0300

    Add menu route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import AppLayout from './pages/AppLayout.jsx';
 import Error from './components/Error.jsx';
 import Home from './pages/Home.jsx';
+import Menu from './pages/menu/Menu.jsx';
+import { loader as menuLoader } from './pages/menu/MenuLoader.jsx';
 
 const router = createBrowserRouter([
   {
@@ -12,6 +14,12 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
       },
     ],
   },
