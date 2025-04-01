1cdb15fb5ec6801e4192038e3a4dc09ec8b693ff
stanislava-pavlovaa <stanislavagpavlova@gmail.com>
2025-04-01 21:13:12 +0300

    Add total cart quantity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { getTotalCartQuantity } from '../pages/cart/cartSlice';
 
 function Header() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+
   return (
     <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
       <Link to="/">TastyTrek</Link>
 
       {/* <SearchOrder /> */}
+
+      <Link to="/cart">{totalCartQuantity} pizzas | 🛒</Link>
     </header>
   );
 }
